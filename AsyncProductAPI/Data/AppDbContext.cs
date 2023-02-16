b45f16abae3d1ad4ab9fe01fe816c28a9fb7ba5c
stescobedo92 <stescobedo.31@gmail.com>
2023-02-16 10:36:38 -0600

    Added the db context class

---
+++ AsyncProductAPI/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using AsyncProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsyncProductAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    DbSet<ListingRequest> ListingRequests => Set<ListingRequest>();
+}
