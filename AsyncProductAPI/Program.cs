03eb4370fa0872602524c386370adf6a54d74eb6
stescobedo92 <stescobedo.31@gmail.com>
2023-02-16 10:39:55 -0600

    Added db contect to the builder service for dependency injection

--- AsyncProductAPI/Program.cs
+++ AsyncProductAPI/Program.cs
@@ -1,4 +1,8 @@
+using AsyncProductAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=RequestDB.db"));
 
 var app = builder.Build();
 
