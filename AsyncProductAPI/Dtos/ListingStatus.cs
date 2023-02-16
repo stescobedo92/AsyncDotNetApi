4e64cdc356f89ecb1b1c19b4116703bc628a676c
stescobedo92 <stescobedo.31@gmail.com>
2023-02-16 11:14:13 -0600

    Added dto for management request status

---
+++ AsyncProductAPI/Dtos/ListingStatus.cs
@@ -0,0 +1,8 @@
+namespace AsyncProductAPI.Dtos;
+
+public class ListingStatus
+{
+    public string? RequestStatus { get; set; }
+    public string? EstimatedCompetionTime { get; set; }
+    public string? ResourceURL { get; set; }
+}
